87feda1c6bca276874f368eb033adc2e8006d177
Victor Astudillo <115374610+V-enekoder@users.noreply.github.com>
2025-06-26 18:12:46 -0400

    Add files via upload

--- src/schema/schemas.go
+++ src/schema/schemas.go
@@ -28,15 +28,25 @@ type Document struct {
 }
 
 type Project struct {
-    gorm.Model
-    UserID    uint     // Clave foránea a User
-    Name      string   `gorm:"type:varchar(255)"`  
-    Address   string   `gorm:"type:text"`           // <- Añade este campo
-    Summary   string   `gorm:"type:text"`           // <- Añade este campo
-    MimeType  string   `gorm:"type:varchar(100)"`   // <- Añade este campo
-    FileSize  float64  // <- Añade este campo
+	gorm.Model
+	UserID   uint    // Clave foránea a User
+	Name     string  `gorm:"type:varchar(255)"`
+	Address  string  `gorm:"type:text"`         // <- Añade este campo
+	Summary  string  `gorm:"type:text"`         // <- Añade este campo
+	MimeType string  `gorm:"type:varchar(100)"` // <- Añade este campo
+	FileSize float64 // <- Añade este campo
 
-    // Relaciones
-    User      User      `gorm:"foreignKey:UserID"` 
-    Documents []Document `gorm:"many2many:document_project;"` // Relación muchos-a-muchos
+	// Relaciones
+	User      User       `gorm:"foreignKey:UserID"`
+	Documents []Document `gorm:"many2many:document_project;"` // Relación muchos-a-muchos
+}
+
+type Chat struct {
+	gorm.Model
+	UserID    uint   `gorm:"not null"`
+	ContextID uint   `gorm:"not null"` // ID del contexto de la conversación
+	Prompt    string `gorm:"type:text;not null"`
+	Response  string `gorm:"type:text;not null"`
+
+	User User `gorm:"foreignKey:UserID"`
 }
